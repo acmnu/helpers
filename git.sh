eaae5f731ee5b07ee988d9e4e89c83d7be8950dc
Anton Chevychalov <pub@mnu.pp.ru>
2017-09-06 17:32:27 +0300

    Add helper that do fetch-rebase-push-pull-request cycle

---
+++ git.sh
@@ -0,0 +1,7 @@
+hlp_hub_fetch_rebase_push_origin () {
+    [[ -z "$1" ]] && echo "origin branch name expected" && return 1
+	git fetch origin || return 1
+    git rebase origin/$1 || return 1
+    git push || return 1
+    hub pull-request -b $1 || return 1
+}
