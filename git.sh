79153396f0c4a6e0fc150b3fa1f46c45b9a6a1c7
Anton Chevychalov <pub@mnu.pp.ru>
2017-12-14 10:27:36 +0300

    Add hub_fetch_rebase

--- git.sh
+++ git.sh
@@ -5,3 +5,8 @@ hlp_hub_fetch_rebase_push_origin () {
     git push || return 1
     hub pull-request -b $1 || return 1
 }
+
+hlp_hub_fetch_rebase () {
+    [[ -z "$1" ]] && echo "origin branch name expected" && return 1
+    git rebase origin/$1 || return 1
+}
