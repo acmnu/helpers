f623ed0d2058b722a02085b4b2641b6ff74599e8
Anton Chevychalov <pub@mnu.pp.ru>
2018-11-12 20:40:27 +0300

    docker helpers refactoring

--- docker.sh
+++ docker.sh
@@ -1,4 +1,16 @@
-hlp_docker_rm_all () {
+hlp_docker_rm_containers () {
 	docker ps -a | grep -v IMAGE | awk '{print $1}' | xargs docker rm 
+}
+
+hlp_docker_rm_all () {
+	hlp_docker_rm_containers
 	docker images | grep -v IMAGE | awk '{print $3}' | xargs docker rmi
+}
+
+hlp_docker_stop_all () {
+	docker ps -a | grep -v IMAGE | awk '{print $1}' | xargs docker stop 
+}
+
+hlp_docker_rm_none_image() {
+    docker images | awk 'NR == 1 {next} /none/ {print $3}' | xargs docker rmi
 }
